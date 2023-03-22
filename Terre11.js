71f6ba45b3abb81024012a74282793057ba08fab
XO <xavierordonneau@gmail.com>
2023-03-22 15:10:40 +0100

    Correction exo 11

--- Terre11.js
+++ Terre11.js
@@ -2,55 +2,71 @@
 Pour déterminer si un nombre est premier, tu dois donc dresser la liste de tous ses diviseurs.
 Un diviseur est un nombre par lequel tu peux effectuer une division et obtenir un nombre entier (sans virgule). */
 
-let listeArgs = process.argv.slice(2);
-let numberOfArgs = listeArgs.length;
-let chiffre1 = 0;
-let result = 0;
-let controleOK = false;
+let inputNumber = 0;
 
-if ((numberOfArgs < 1) || (numberOfArgs > 1)) {
-    console.log("Un seul argument obligatoire");
-} else {
-    chiffre1 = listeArgs[0];
-
-    if (isNaN(chiffre1)) {
-        console.log("L'argument n'est pas un nombre")
-    } else if (chiffre1 < 0) {
-        console.log("L'argument doit être positif")
-    } else if ((chiffre1 == 0) || (chiffre1 == 1)) {
-        console.log(chiffre1 + " n'est pas un nombre entier")
+let inputControl = (listeArgs) => {
+    let numberOfArgs = listeArgs.length;
+    if ((numberOfArgs < 1) || (numberOfArgs > 1)) {
+        console.log("Enter a Number like 25");
+        return false;
     } else {
-
-        //on le divise dabord par 1 et lui même
-        result = chiffre1 / 1
-        if (Number.isInteger(result)) {
-            result = chiffre1 / chiffre1
-            if (Number.isInteger(result)) {
-                controleOK = true;
-            }
-        }
-
-        //ensuite on contrôle si on peut le diviser par un autre nombre que 1 et lui-même
-        if (controleOK == true) {
-            i = chiffre1 - 1
-            while (i > 1) { 
-                result = chiffre1 / i
-                if (Number.isInteger(result)) {
-                    //on peut le diviser par un autre nombre que lui même et 1 donc ce n'est PAS un nombre entier
-                    //inutile de contrôler le reste
-                    controleOK = false;
-                    //console.log(chiffre1 + "divisé par " + i + " = " + result)
-                    break;
-                } else {
-                    //console.log("ok : " + chiffre1 + "divisé par " + i + " = " + result)
-                }
-                i--;
-            }
-        }
-        if (controleOK == true) {
-            console.log(chiffre1 + " est un nombre entier");
-        } else {
-            console.log(chiffre1 + " n'est pas un nombre entier");
+        inputNumber = parseInt(listeArgs[0]);
+        if (isNaN(inputNumber)) {
+            console.log(`${listeArgs[0]} is not a number`);
+            return false;
+        } else if (inputNumber < 0) {
+            console.log("You have to input positive number");
+            return false;
+        } else if ((inputNumber == 0) || (inputNumber == 1)) {
+            console.log(`${inputNumber} is not a primary number`);
+            return false;
         }
     }
+    return true;
+};
+
+let isDividableByItselfAndOne = (inputNumber) => {
+    //le chiffre est divisible par 1 et lui-même
+    let result;
+    result = inputNumber / 1;
+    if (Number.isInteger(result)) {
+        result = inputNumber / inputNumber;
+        if (Number.isInteger(result)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+let isPrimaryNumber = (inputNumber) => {
+    let result;
+    let i;
+
+    if (!isDividableByItselfAndOne(inputNumber)) {
+        return false;
+    }
+
+    i = inputNumber - 1
+    while (i > 1) {
+        result = inputNumber / i;
+        if (Number.isInteger(result)) {
+            //on peut le diviser par un autre nombre que lui même et 1 donc ce n'est PAS un nombre entier
+            //inutile de contrôler le reste
+            return false;
+        }
+        i--;
+    }
+    return true;
+};
+
+if ( inputControl(process.argv.slice(2)) ) {
+    if ( isPrimaryNumber(inputNumber) ) {
+        console.log("C'est un nombre premier");
+    } else {
+        console.log("Ce n'est pas un nombre premier");
+    }
 }
+
+
+
+
